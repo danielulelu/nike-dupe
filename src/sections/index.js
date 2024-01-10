a150f0e8e08764a20c7957494c168fcb95f7cf25
Eleven <danielulelu@gmail.com>
2024-01-10 19:11:06 +0300

    initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,12 @@
+import PopularProducts from "./PopularProducts";
+import Hero from "./Hero";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+
+export {
+    PopularProducts, Hero, SpecialOffer, Footer, Subscribe, CustomerReviews, SuperQuality, Services
+}
